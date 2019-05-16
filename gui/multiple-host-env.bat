00d290425a5b3b8cbf3ba3e2e18f9588dafbceb3
nick <nick@haimi.com>
2019-05-16 13:12:25 +0800

    test

--- gui/multiple-host-env.bat
+++ gui/multiple-host-env.bat
@@ -1,2 +1,2 @@
-go build -ldflags="-H windowsgui" -o ../multiple-host-env-gui.exe multiple-host-env.go
+go build -ldflags="-H windowsgui -s -w" -o ../multiple-host-env-gui.exe multiple-host-env.go
 upx -9 upx --brute -9 ../multiple-host-env-gui.exe
